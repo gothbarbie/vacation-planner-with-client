871d586bcc4e1f10fdf606150e0056e0844ec520
Hanna Söderström <gothbarbie84@gmail.com>
2018-03-02 15:14:30 +0100

    fix lint errors

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,6 +1,6 @@
 import { FETCH_USER } from '../actions/types'
 
-export default function(state = null, action) {
+export default function (state = null, action) {
   switch (action.type) {
     case FETCH_USER: 
       return action.payload || false
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,5 +3,5 @@ import { combineReducers } from 'redux'
 import authReducer from './authReducer'
 
 export default combineReducers({
-  auth: authReducer
+  auth: authReducer,
 })
