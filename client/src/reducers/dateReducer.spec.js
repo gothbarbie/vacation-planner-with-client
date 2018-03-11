baaedac17d748d1b707ae4deea61c765116df2fe
Hanna Söderström <gothbarbie84@gmail.com>
2018-03-11 14:37:06 +0100

    Add mockDate for dateReducer to avoid real Date from changing between test occations

--- client/src/reducers/dateReducer.spec.js
+++ client/src/reducers/dateReducer.spec.js
@@ -2,16 +2,32 @@ import dateReducer from './dateReducer'
 import { SET_DATE } from '../actions/types'
 
 describe('reducers/dateReducer', () => {
-  it('returns current date as default', () => {
-    const date = new Date()
-    expect(dateReducer(undefined, {})).toEqual(date)
+  const RealDate = Date
+
+  function mockDate (isoDate) {
+    global.Date = class extends RealDate {
+      constructor () {
+        return new RealDate(isoDate)
+      }
+    }
+  }
+
+  afterEach(() => {
+    global.Date = RealDate
   })
-  
+
+  it('returns current date as default', () => {
+    mockDate('2017-11-25T12:34:56z')
+    expect(dateReducer(undefined, {})).toEqual(new Date())
+  })
+
   it('sets a date', () => {
     const date = new Date(1984, 2, 2)
-    expect(dateReducer(undefined, {
-      type: SET_DATE,
-      payload: date,
-    })).toEqual(date)
+    expect(
+      dateReducer(undefined, {
+        type: SET_DATE,
+        payload: date,
+      })
+    ).toEqual(date)
   })
 })
