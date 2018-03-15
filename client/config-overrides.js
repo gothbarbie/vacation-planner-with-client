034be47b970c1a8795ea2a51b81ef1d576bb1d7a
Hanna Söderström <gothbarbie84@gmail.com>
2018-03-15 14:33:22 +0100

    Install react-app-rewire and its plugin for styled-components

---
+++ client/config-overrides.js
@@ -0,0 +1,11 @@
+/* config-overrides.js */
+const rewireStyledComponents = require('react-app-rewire-styled-components')
+
+module.exports = function override (config, env) {
+  //do stuff with the webpack config...
+  config = rewireStyledComponents(config, env, {
+    ssr: true,
+    displayName: true,
+  })
+  return config
+}
